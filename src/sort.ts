d7fbdb78dcdd1e0e80ead65638a7c87a5f61825a
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2021-11-07 16:50:55 +0100

    🎨 Work around type narrowing limitation
    It was not `Nag`, it was `Object.entries()`

--- src/sort.ts
+++ src/sort.ts
@@ -1,15 +1,16 @@
-import { Nag } from './errors';
-import { VCard4, SingleVCardProperty, isKnownProperty } from './vcard4Types';
-
-// For some reason, the compiler believes that `Nag` could be a type passed
-function PREFValue(a: SingleVCardProperty<any> | Nag<any>): number {
-  return (a as SingleVCardProperty<any>).parameters?.PREF || 100;
-}
+import { VCard4, isKnownProperty } from './vcard4Types';
 
 export function sortByPREF<T extends Partial<VCard4>>(vcard: T) {
-  for (const [k, v] of Object.entries(vcard)) {
-    if (isKnownProperty(k) && Array.isArray(v)) {
-      v.sort((a, b) => PREFValue(a) - PREFValue(b));
+  for (const k of Object.keys(vcard)) {
+    // Cannot use `for (const [k, v] of Object.entries(vcard))` due to
+    // https://github.com/microsoft/TypeScript/issues/46718
+    if (isKnownProperty(k)) {
+      const v = vcard[k];
+      if (Array.isArray(v)) {
+        v.sort(
+          (a, b) => (a.parameters?.PREF || 100) - (b.parameters?.PREF || 100),
+        );
+      }
     }
   }
   if ('x' in vcard) {
